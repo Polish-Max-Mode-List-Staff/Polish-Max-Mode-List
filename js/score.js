2bce46bc10a2071effa0d0f274e919169aee9a01
Xenomical <238035250+Xenomical@users.noreply.github.com>
2025-10-15 11:53:56 +0200

    Scoring update

--- js/score.js
+++ js/score.js
@@ -6,35 +6,15 @@ const scale = 3;
 /**
  * Calculate the score awarded when having a certain percentage on a list level
  * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    if (rank > 100) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
-
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    score = Math.max(0, score);
-
-    if (percent != 100) {
-        return round(score - score / 3);
-    }
-
-    return Math.max(round(score), 0);
-}
+    
+    score = Math.max(5,Math.min(1000,round(5+995*((101-rank)/100)**2)))
+    return score
 
 export function round(num) {
     if (!('' + num).includes('e')) {
