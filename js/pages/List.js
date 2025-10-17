aff548e3c8e59898d664c031442d864e38b709fe
Xenomical <238035250+Xenomical@users.noreply.github.com>
2025-10-17 12:03:05 -0700

    h

--- js/pages/List.js
+++ js/pages/List.js
@@ -56,7 +56,7 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                    <p v-if="selected + 1 <= 150">This level does not accept new records.</p>
+                    <p v-if="selected + 1 >= 150">This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
                             <td class="date">
