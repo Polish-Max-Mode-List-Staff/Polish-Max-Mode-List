f11beda3e7508956a0ddd27b8fb0d08d392dd3d2
Xenomical <238035250+Xenomical@users.noreply.github.com>
2025-10-17 11:29:37 -0700

    beeep beep boop bop

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,10 +1,10 @@
 export default {
     props: {
-        author: {
+        developers: {
             type: String,
             required: true,
         },
-        creators: {
+        game: {
             type: Array,
             required: true,
         },
@@ -12,48 +12,46 @@ export default {
             type: String,
             required: true,
         },
+        dateVerified: {
+            type: String,
+            required: true,
+        },
     },
     template: `
         <div class="level-authors">
             <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
+                <div class="type-title-sm">Developer & Verifier</div>
                 <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-            </template>
-            <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
+                    <span>{{ developers }}</span>
                 </p>
             </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
+                <div class="type-title-sm">Developer(s)</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                    <template v-for="(developer, index) in developers" :key="\`developer-\$\{developer\}\`">
+                        <span >{{ developers }}</span
+                        ><span v-if="index < developers.length - 1">, </span>
                     </template>
+                </p>
+                <div class="type-title-sm">Gane</div>
+                <p class="type-body">
+                    <span>{{ game }}</span>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm">Date Verified</div>
             <p class="type-body">
-                <span>{{ author }}</span>
+                <span>{{ dateVerified }}</span>
             </p>
         </div>
     `,
 
     computed: {
         selfVerified() {
-            return this.author === this.verifier && this.creators.length === 0;
+            return this.developers === this.verifier;
         },
     },
 };
